7daad34b315d3502b907040eed44fbceaf140f38
Jesper Rage <aaxbox@hotmail.com>
2015-11-11 14:17:57 -0800

    Added INSTALL.txt Added Default VS project

---
+++ VSProj/VSProj/VSProj.cpp
@@ -0,0 +1,14 @@
+// VSProj.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	cout << "Hello\n";
+    return 0;
+}
+
