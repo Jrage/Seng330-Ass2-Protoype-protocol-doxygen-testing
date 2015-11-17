be119dd7457862e6256a683e1be3c8aa44a8b91b
Jesper Rage <aaxbox@hotmail.com>
2015-11-16 16:31:28 -0800

    Finished Serializing and un serializing. Updated execution structure.

--- VSProj/VSProj/VSProj.cpp
+++ VSProj/VSProj/VSProj.cpp
@@ -4,6 +4,8 @@
 
 #include "stdafx.h"
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <string>
 #include <vector>
 #include "rapidjson/document.h"
@@ -27,16 +29,9 @@ public:
 
 		str.SetString(name.c_str(), name.length(), d.GetAllocator());
 		d.AddMember("name", str, d.GetAllocator());
+		str.SetString(this->getEquipmentType().c_str(), getEquipmentType().length(), d.GetAllocator());
+		d.AddMember("instanceType", str, d.GetAllocator());
 
-		str.SetString(getEquipmentType().c_str(), getEquipmentType().length(), d.GetAllocator());
-		d.AddMember("instanceName", str, d.GetAllocator());
-
-		/*
-		rapidjson::StringBuffer buf;
-		rapidjson::Writer<rapidjson::StringBuffer> writer(buf);
-		d.Accept(writer);
-		cout << buf.GetString() << "\n";
-		*/
 		return d;
 	}
 	string name = "Unnamed";    /*!< Name of equipments instance, by default equipment is unnamed */
@@ -75,7 +70,7 @@ class Factory {
 	class Benchpress : public Equipment {
 	public:
 		Equipment*   clone() { return new Benchpress; };  /*!< Returns a clone */
-		string getEquipmentType() { return "Benchpress"; }
+		string getEquipmentType() { return "BenchPress"; }
 
 		void printEquipment() {
 			cout << "Benchpress: " << name << "\n";
@@ -91,7 +86,11 @@ class Factory {
 		return item;
 	}
 
-
+	//! Reads the given list of equipment, serialises the objects and saves to the given file
+	/*!
+	\param list of equipment to become serialized data
+	\param file name of file to save the serialized data to
+	*/
 	void serializeData(vector <Equipment*> list, string file) {
 		rapidjson::Document d;
 		d.SetObject();
@@ -99,7 +98,8 @@ class Factory {
 
 		rapidjson::Value array(rapidjson::kArrayType);
 		for (int i = 0; i < list.size(); ++i) {
-			array.PushBack(list[i]->serialize(), allocator) ;
+			rapidjson::Value val(list[i]->serialize(), allocator); // need to copy
+			array.PushBack(val, allocator) ;
 		}
 
 		d.AddMember("equipment", array, allocator);
@@ -107,12 +107,53 @@ class Factory {
 		rapidjson::StringBuffer buf;
 		rapidjson::Writer<rapidjson::StringBuffer> writer(buf);
 		d.Accept(writer);
-		cout << buf.GetString() << "\n";
+		cout << "File Saved\n";
 
+		ofstream myfile;
+		myfile.open(file);
+		myfile << buf.GetString();
+		myfile.close();
 	}
 
+	//! Reads the given file of serialized data, creates the objects and returns them as if they have been created manually
+	/*!
+	\param file name of file with the serialized data
+	\return the new list of prototyped object created from the serialized data.
+	*/
 	vector <Equipment*> loadSerializedData(string file) {
+
+		ifstream inFile;
+		inFile.open(file);//open the input file
+
+		stringstream strStream;
+		strStream << inFile.rdbuf();//read the file
+		string str = strStream.str();//str holds the content of the file
+		inFile.close();
+	
+
+		rapidjson::Document d;
+		d.Parse(str.c_str());
+
 		vector<Equipment*> list;
+
+		const rapidjson::Value& b = d["equipment"];
+
+		for (rapidjson::SizeType i = 0; i < b.Size(); i++)
+		{
+			const rapidjson::Value& c = b[i];
+			//printf("%s, %s \n", c["name"].GetString(), c["instanceType"].GetString());
+			int choice;
+			if (strcmp( c["instanceType"].GetString() , "LegPress") == 0)
+				choice = 1;
+			else if (strcmp(c["instanceType"].GetString() , "BenchPress") == 0)
+				choice = 2;
+			else 
+				continue;
+			
+			list.push_back(Factory::make_Equipment(choice, c["name"].GetString()));
+		}
+
+		cout << "File Loaded\n";
 		return list;
 	}
 
@@ -125,7 +166,7 @@ class Factory {
 		int             choice;
 
 		while (true) {
-			cout << "(0) finish,\n(1) Add Legpress,\n(2) Add Benchpress,\n(3) Save Serialized Data,\n(4) Load Serialized Data,\n(5) print Equipment\nenter( 0,1,2,3,4,5) :";
+			cout << "\n(0) finish,\n(1) Add Legpress,\n(2) Add Benchpress,\n(3) Save Serialized Data,\n(4) Load Serialized Data,\n(5) print Equipment\nenter( 0,1,2,3,4,5) :";
 			cin >> choice;
 			
 			//Cancel
@@ -142,20 +183,18 @@ class Factory {
 
 			//Print
 			else if ( choice == 5){
-
+				cout << "\n";
 				for (int i = 0; i < list.size(); ++i) {
 					list[i]->printEquipment();
-					cout << list[i]->name;
 				}
 			
 			// Manually add
 			}else {
 				string name;
-				cout << "Enter Instance Name (no spaces): ";
+				cout << "\nEnter Instance Name (no spaces): ";
 				cin >> name;
 
-				list.push_back(
-					Factory::make_Equipment(choice, name));
+				list.push_back(Factory::make_Equipment(choice, name));
 			}
 		}
 
